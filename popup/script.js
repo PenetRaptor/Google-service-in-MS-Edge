ffb1f37ed08dc26235724ecc6fbc562363fa055e
PenetRaptor <skyjammer159@gmail.com>
2021-02-10 12:20:06 +0700

    Начнем учится строить будущее!
    Signed-off-by: PenetRaptor <skyjammer159@gmail.com>

---
+++ popup/script.js
@@ -0,0 +1,45 @@
+const newTab_Docks = document.getElementById("newTab_Docks");
+newTab_Docks.onclick = function() {
+    window.open('https://docs.google.com/document/u/0/');
+};
+
+const newTab_Table = document.getElementById("newTab_Table");
+newTab_Table.onclick = function() {
+    window.open('https://docs.google.com/spreadsheets/u/0/');
+};
+
+const newTab_Presentation = document.getElementById("newTab_Presentation");
+newTab_Presentation.onclick = function() {
+    window.open('https://docs.google.com/presentation/u/0/');
+};
+
+const newTab_Gmail = document.getElementById("newTab_Gmail");
+newTab_Gmail.onclick = function() {
+    window.open('https://mail.google.com/');
+};
+
+const newTab_Drive = document.getElementById("newTab_Drive");
+newTab_Drive.onclick = function() {
+    window.open('https://drive.google.com/');
+};
+
+const newTab_Translate = document.getElementById("newTab_Translate");
+newTab_Translate.onclick = function() {
+    window.open('https://translate.google.ru/');
+};
+
+const newTab_Class = document.getElementById("newTab_Class");
+newTab_Class.onclick = function() {
+    window.open('https://classroom.google.com/');
+};
+
+const newTab_Maps = document.getElementById("newTab_Maps");
+newTab_Maps.onclick = function() {
+    window.open('https://www.google.ru/maps/');
+};
+
+const newTab_News = document.getElementById("newTab_News");
+newTab_News.onclick = function() {
+    window.open('https://news.google.com/');
+};
+
